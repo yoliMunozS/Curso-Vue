8cc40ba670fb1dbd8a4e22efa30fa7c6cc0b3a4a
yoliMunozS <33128702+yoliMunozS@users.noreply.github.com>
2022-05-30 18:15:28 +0200

    vue-S4.Movies

--- nivell-1/src/films.js
+++ nivell-1/src/films.js
@@ -1,28 +1,62 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-
+  
   const result = array.map((item) => item.director);
   console.log("EXERCICE 1 ->", result);
   return result;
 }
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  
+  const result = array.filter((item) => item.director === director);
+  console.log("EXERCICE 2 ->", result);
+  return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
   
+  const buscarDirector = array.filter((item) => item.director === director);
+  const buscarScore= buscarDirector.map((item) => item.score);
+  const result = buscarScore.reduce((acumulador, valorActual) => acumulador + valorActual).toFixed(2);
+  const resultFinal = (result / buscarScore.length).toFixed(2);
+  console.log("EXERCICE 3 ->", resultFinal);
+  return resultFinal;
 }
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
   
+  const buscarTitle = array.map((item) => item.title);
+  const tituloOrdenado = buscarTitle.sort();
+  const result = tituloOrdenado.slice(0,20);
+  console.log("EXERCICE -> 4", result);
+  return result;
 }
 
 // Exercise 5: Order by year, ascending
 function orderByYear() {
-
+  
+  const movies = [];
+  const arrayMovies = movies.map(({title, year}) => ({title, year}));
+ 
+  const ordenarYear = arrayMovies.sort((a, b) => {
+      if (a.year > b.year) {
+        return -1;
+      }
+      if (a.year < b.year) {
+        return 1;
+      }
+      if (a.title < b.title) {
+        return -1;
+      }
+      if (a.title > b.title) {
+        return 1;
+        }
+    return 0;
+  });
+  console.log("EXERCICE -> 5", ordenarYear);
+  return ordenarYear;
 }
 
 // Exercise 6: Calculate the average of the movies in a category
